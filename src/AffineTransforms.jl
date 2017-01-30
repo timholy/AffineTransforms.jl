5289aa84cb020ced0cc34fce3901f5e58fb370ac
Tim Holy <tim.holy@gmail.com>
2017-01-30 14:51:43 -0600

    Fix deprecation warnings from Images 0.6.0

--- src/AffineTransforms.jl
+++ src/AffineTransforms.jl
@@ -1,4 +1,4 @@
-VERSION >= v"0.4.0-dev+6521" && __precompile__(true)
+__precompile__(true)
 
 module AffineTransforms
 
@@ -7,10 +7,6 @@ using Interpolations, Requires, Base.Cartesian
 
 if !isdefined(:AbstractVecOrMat)
     typealias AbstractVecOrMat Union(AbstractVector, AbstractMatrix)
-end
-
-if VERSION < v"0.5.0"
-    using Compat
 end
 
 export
