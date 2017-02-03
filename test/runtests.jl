8cdf51aaac02d72762dd129a97d8092433fa2b41
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2017-02-03 21:16:07 +0000

    Seems Unitful is not imported when in testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -224,8 +224,8 @@ end
     @test At[:,1:2] == A[:,2:3]
 end
 
+using Unitful: @u_str
 @testset "With separates types for linear transform and translation" begin
-	using Unitful: @u_str
-    aff = AffineTransforms.AffineTransform(Val{:units}, [0 -1; 1 0], [2, 3]u"nm")
+    aff = AffineTransforms.AffineTransform([0 -1; 1 0], [2, 3]u"nm")
     @test aff * ([1, 2]u"nm") == [0, 4]u"nm"
 end
